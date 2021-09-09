211bfe0107568ef04ddb84a71f873f68d8cf70a3
nguyentanvinh7a <43849741+nguyentanvinh7a@users.noreply.github.com>
2021-09-09 17:10:04 +0700

    first commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,39 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+TestjuceAudioProcessorEditor::TestjuceAudioProcessorEditor (TestjuceAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    mLoadButton.onClick = [&]() {audioProcessor.loadFile(); };
+    addAndMakeVisible(mLoadButton);
+    setSize (200, 200);
+}
+
+TestjuceAudioProcessorEditor::~TestjuceAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void TestjuceAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void TestjuceAudioProcessorEditor::resized()
+{
+    mLoadButton.setBounds(getWidth() / 2 - 50, getHeight() / 2 - 50, 100, 100);
+}
